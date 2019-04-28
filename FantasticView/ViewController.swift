3d31a5ccedb80395f1ba74232a47e933a8a4910e
Apple_Hao <apple_hao@apple-hao.local>
2019-04-28 13:51:58 +0800

    Initial Commit

---
+++ FantasticView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FantasticView
+//
+//  Created by Apple_Hao on 2019/4/28.
+//  Copyright © 2019年 Apple_Hao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
